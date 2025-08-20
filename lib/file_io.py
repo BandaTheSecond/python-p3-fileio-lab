5eade0dcab6ceb5c8ff8203fc29b022798251cfd
Kingbanda <bandadesignske@gmail.com>
2025-08-21 00:11:09 +0300

    File io lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
 def write_file(file_name, file_content):
-    pass
+     
+     with open(f"{file_name}.txt", "w", encoding="utf-8") as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+
+    with open(f"{file_name}.txt", "a", encoding="utf-8") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    try:
+        with open(f"{file_name}.txt", "r", encoding="utf-8") as file:
+            return file.read()
+    except FileNotFoundError:
+        return None
+    except Exception as e:
+        print(f"An error occurred while reading the file: {e}")
+        return None
